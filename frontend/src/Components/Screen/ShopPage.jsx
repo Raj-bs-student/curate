af0dd1f00686451c1d8f7605ca3c1dc07048be17
Raj <raj1512@pm.me>
2025-08-11 15:05:24 +0530

    feat: Add ShopPage component

---
+++ frontend/src/Components/Screen/ShopPage.jsx
@@ -0,0 +1,14 @@
+import UpperBorder from '../Micro/UpperBorder.jsx';
+import ProductCard from '../Main/ProductCard.jsx';
+import ButtonMinimalist from '../Micro/ButtonMinimalist.jsx';
+import Header from '../Main/Header.jsx';
+
+const ShopPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default ShopPage
